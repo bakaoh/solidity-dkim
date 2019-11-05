72e438b740184ab3ad903afd6eb54c8f3db57b08
bakaking <tatattai@yahoo.com>
2019-11-05 17:03:42 +0700

    Various

--- contracts/DKIM.sol
+++ contracts/DKIM.sol
@@ -20,7 +20,8 @@ contract DKIM {
         strings.slice canonicalHeader,
         strings.slice canonicalBody,
         strings.slice hashAlgorithm,
-        strings.slice verifyAlgorithm
+        strings.slice verifyAlgorithm,
+        strings.slice headers
     ) {
         signature.split(": ".toSlice());
         var sdelim = ";".toSlice();
@@ -39,20 +40,72 @@ contract DKIM {
             } else if (tagname.endsWith("a".toSlice())) {
                 verifyAlgorithm = spart.split("-".toSlice());
                 hashAlgorithm = spart;
-            } 
+            } else if (tagname.endsWith("bh".toSlice())) {
+            } else if (tagname.endsWith("h".toSlice())) {
+                headers = spart;
+            }
         }
+    }
+
+    function parseHeader(strings.slice value) private pure returns (strings.slice[]) {
+        var delim = ":".toSlice();
+        var headers = new strings.slice[](value.count(delim) + 1);
+        for(uint i = 0; i < headers.length; i++) {
+            headers[i] = value.split(delim);
+        }
+        return headers;
     }
 
     function processBody(strings.slice message, strings.slice method) internal pure returns (
         string
     ) {
+        var crlf = "\r\n".toSlice();
+        if (method.equals("relaxed".toSlice())) {
+            // Ignore all whitespace at the end of lines.
+            while (message.contains("\x20\r\n".toSlice())) {
+                var h = message.split("\x20\r\n".toSlice());
+                message = h.concat(crlf).toSlice().concat(message).toSlice();
+            }
 
+            // Reduce all sequences of WSP within a line to a single SP
+        }
+
+        // Ignore all empty lines at the end of the message body.
+        var emptyLines = "\r\n\r\n".toSlice();
+        while (message.endsWith(emptyLines)) {
+            message._len -= crlf._len;
+        }
+        return message.toString();
     }
 
-    function getLen(string memory value) public pure returns (bytes32) {
+    function getHeader(strings.slice allHeaders, strings.slice name) internal pure returns (
+        strings.slice
+    ) {
+        var delim = "\r\n".toSlice();
+        var count = allHeaders.count(delim) + 1;
+        for(uint i = 0; i < count; i++) {
+            var part = allHeaders.split(delim);
+            var lowercase = _toLower(part.toString()).toSlice();
+            if (lowercase.startsWith(name)) {
+                var value = part;
+                for (i = i + 1; i < count; i++) {
+                    var part2 = allHeaders.split(delim);
+                    if (part2.startsWith("\x20".toSlice())) {
+                        value = value.concat(delim).toSlice().concat(part2).toSlice();
+                    } else {
+                        return value;
+                    }
+                }
+            }
+        }
+        return "".toSlice();
+    }
+
+    function getLen(string memory value) public returns (bytes32) {
         var body = value.toSlice();
         var header = body.split("\r\n\r\n".toSlice());
-
+        var allHeader = header.copy();
+        // return getHeader(allHeader, _toLower("DKIM-Signature").toSlice()).toString();
         var delim = "\r\n".toSlice();
         var count = header.count(delim) + 1;
         for(uint i = 0; i < count; i++) {
@@ -64,28 +117,47 @@ contract DKIM {
                     if (part2.startsWith("\x20".toSlice())) {
                         signature = signature.concat(part2).toSlice();
                     } else {
-                        // // parse signature
-                        // signature.split(": ".toSlice());
-                        // var sdelim = ";".toSlice();
-                        // var scount = signature.count(sdelim) + 1;
+                        var (,,,,,,ha) = parse(signature);
 
-                        // var domain; 
-                        // var selector;
-                        // for (uint j = 0; j < scount; j++) {
-                        //     var spart = signature.split(sdelim);
-                        //     var tagname = spart.split("=".toSlice());
-                        //     if (tagname.endsWith("d".toSlice())) {
-                        //         return spart.toString();
-                        //     }
-                        // }
-
-                        var (, selector) = parse(signature);
-                        bytes32 h = sha256(bytes(body.toString()));
+                        // hash
+                        bytes32 h = sha256(bytes(processBody(body, body)));
                         return h;
+                        
+                        // var headers = parseHeader(h);
+                        // for (uint j = 0; j < headers.length; j++) {
+                        //     headers[j] = getHeader(allHeader.copy(), headers[j]);
+                        // }
+                        // return headers[0].toString();
+                        // var count3 = header3.count(delim) + 1;
+                        // for(uint i3 = 0; i3 < count; i3++) {
+                        //     var part3 = header3.split(delim);
+                        //     var headerName = _toLower(part3.split(":".toSlice()).toString());
+                        //     for (uint j3 = 0; j3 < headers.length; j3++) {
+                        //         if (headers[j3].equals(headerName.toSlice())) {
+                        //             headers[j3] = part3;
+                        //         }
+                        //     }
+
+                        // }
                     }
                 }
             }
         }
         return "notfound";
     }
+
+    function _toLower(string str) internal pure returns (string) {
+		bytes memory bStr = bytes(str);
+		bytes memory bLower = new bytes(bStr.length);
+		for (uint i = 0; i < bStr.length; i++) {
+			// Uppercase character...
+			if ((bStr[i] >= 65) && (bStr[i] <= 90)) {
+				// So we add 32 to make it lowercase
+				bLower[i] = bytes1(int(bStr[i]) + 32);
+			} else {
+				bLower[i] = bStr[i];
+			}
+		}
+		return string(bLower);
+	}
 }
--- src/test.js
+++ src/test.js
@@ -1,5 +1,5 @@
 var dns = require("dns");
-var crypto = require( 'crypto' );
+var crypto = require("crypto");
 
 // let domain = "google._domainkey.fusemachines.com";
 // dns.resolve( domain, 'TXT', function( error, records ) {
@@ -39,34 +39,157 @@ header.split(/\r\n(?=[^\x20\x09]|$)/g).forEach(function(h, i, headers) {
 
 // console.log(signatures)
 
-function processBody( message, method ) {
+function processBody(message, method) {
+  method = method || "simple";
 
-    method = method || 'simple'
-  
-    if( method !== 'simple' && method !== 'relaxed' ) {
-      throw new Error( 'Canonicalization method "' + method + '" not supported' )
-    }
-  
-    // @see https://tools.ietf.org/html/rfc6376#section-3.4.3
-    if( method === 'simple' ) {
-      return message.toString( 'ascii' )
-        .replace( /(\r\n)+$/m, '' ) + '\r\n'
-    }
-  
-    // @see https://tools.ietf.org/html/rfc6376#section-3.4.4
-    if( method === 'relaxed' ) {
-      return message.toString( 'ascii' )
-        // Ignore all whitespace at the end of lines.
-        .replace( /[\x20\x09]+(?=\r\n)/g, '' )
-        // Reduce all sequences of WSP within a line to a single SP
-        .replace( /[\x20\x09]+/g, ' ' )
-        // Ignore all empty lines at the end of the message body.
-        .replace( /(\r\n)+$/, '\r\n' )
-    }
-  
+  if (method !== "simple" && method !== "relaxed") {
+    throw new Error('Canonicalization method "' + method + '" not supported');
   }
 
-var digest = crypto.createHash( "sha256" )
-      .update( processBody(body, 'relaxed') )
-      .digest();
-console.log("digest", digest.toString('hex'));
+  // @see https://tools.ietf.org/html/rfc6376#section-3.4.3
+  if (method === "simple") {
+    return message.toString("ascii").replace(/(\r\n)+$/m, "") + "\r\n";
+  }
+
+  // @see https://tools.ietf.org/html/rfc6376#section-3.4.4
+  if (method === "relaxed") {
+    return (
+      message
+        .toString("ascii")
+        // Ignore all whitespace at the end of lines.
+        .replace(/[\x20\x09]+(?=\r\n)/g, "")
+        // Reduce all sequences of WSP within a line to a single SP
+        .replace(/[\x20\x09]+/g, " ")
+        // Ignore all empty lines at the end of the message body.
+        .replace(/(\r\n)+$/, "\r\n")
+    );
+  }
+}
+
+function processHeader(headers, signHeaders, method) {
+  if (typeof signHeaders === "string") {
+    method = signHeaders;
+    signHeaders = null;
+  }
+
+  method = method || "simple";
+
+  if (method !== "simple" && method !== "relaxed") {
+    throw new Error('Canonicalization method "' + method + '" not supported');
+  }
+
+  if (signHeaders != null) {
+    // Clone this array so that newely added headers don't show up outsite this "processHeader" function
+    // See https://tools.ietf.org/html/rfc5322#section-3.6
+    signHeaders = signHeaders.slice();
+    signHeaders.push("DKIM-Signature");
+    signHeaders.push("X-Google-DKIM-Signature");
+
+    signHeaders = signHeaders.map(function(header) {
+      return header.toLowerCase();
+    });
+
+    // Remove duplicates
+    // signHeaders = signHeaders.reduce((ac, val) => [...ac, ...ac.includes(val) ? [] : [val]], [])
+    signHeaders = signHeaders.reduce(function(ac, val) {
+      if (ac.indexOf(val) < 0) {
+        ac.push(val);
+      }
+      return ac;
+    }, []);
+
+    // Sort elements of headers array using the "signHeaders" order
+    var indexedHeaders = headers.map(function(header) {
+      var key = header
+        .slice(0, header.indexOf(":"))
+        .trim()
+        .toLowerCase();
+      var idx = signHeaders.indexOf(key);
+      return { idx, header };
+    });
+
+    headers = indexedHeaders
+      .filter(function(h) {
+        return h.idx > -1;
+      })
+      .sort(function(h1, h2) {
+        return h1.idx - h2.idx;
+      })
+      .map(function(h) {
+        return h.header;
+      });
+
+    // headers = headers.filter( function( header ) {
+    //   var key = header.slice( 0, header.indexOf( ':' ) ).trim().toLowerCase()
+    //   return signHeaders.indexOf( key ) !== -1
+    // })
+  }
+
+  if (method === "simple") {
+    return headers.join("\r\n");
+  }
+
+  // TODO: Something's not right here...
+  // relaxed signatures still don't verify
+  if (method === "relaxed") {
+    return headers
+      .map(function(line) {
+        var lines = {};
+        var colon = line.indexOf(":");
+        var value = line.slice(colon);
+
+        // Convert all header field names to lowercase
+        var key = line.slice(0, colon).toLowerCase();
+
+        // Unfold all header field continuation lines
+        value = value.replace(/\r\n(?=[\x20\x09])/g, "");
+        // Convert all sequences of one or more WSP characters to a single SP
+        value = value.replace(/[\x20\x09]+/g, " ");
+        // Devare all WSP characters at the end of each unfolded header field
+        value = value.replace(/[\x20\x09]+$/g, "");
+
+        // Remove signature value for "dkim-signature" header
+        if (/^(dkim-signature|x-google-dkim-signature)/i.test(key)) {
+          value = value.replace(/ b=([^;]*)/, " b=");
+        }
+
+        if (key === "x-google-dkim-signature") {
+          key = key.replace("x-google-dkim-signature", "dkim-signature");
+        }
+
+        // Remove any WSP characters remaining before and after the colon
+        return (key + value).replace(/[\x20\x09]*[:][\x20\x09]*/, ":");
+      })
+      .join("\r\n");
+  }
+}
+
+var digest = crypto
+  .createHash("sha256")
+  .update(processBody(body, "relaxed"))
+  .digest();
+// console.log("digest", digest.toString("hex"));
+
+var processedHeader = processHeader(
+  signatures[0],
+  ["mime-version", "from", "date", "message-id", "subject", "to"],
+  "simple"
+);
+console.log(processedHeader);
+
+var pubKey =
+  "-----BEGIN PUBLIC KEY-----\n" +
+  "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCRV9r/XrhF3yRvXjFRRP8RKsT3yqVVrZGFYgsKLl/7exRJJBfIBPI+nRzpC1pu5XGUZaheGtj/m1WDU9TrFK4wIvLvKyX65eePw3wNsUMVJP76baeDtilQaUk55iPKq3hzoRDP+buEj0Plivz8sU3lSvTx/Tk54kcsa5UU8XTpVQIDAQAB" +
+  "\n-----END PUBLIC KEY-----";
+
+var verified = crypto
+  .createVerify("RSA-SHA256")
+  .update(processedHeader)
+  .verify(
+    pubKey,
+    Buffer.from("iPc3RHh9oXL6+dvuPM0hYt1vdj6U4hN83BFxhumWsSXnFDFmbSG4OtXHPF823HoZAA" +
+      "4MbFQu5VgfvAQ+FmnKyfON2WdJrAYicyslVXlcA6l0UKSGIH/0NHSqi/kX+4KEKaClY7" +
+      "jZkXZZ8EIl5IUBdRRUWSsySFOtrQ/9IeAb6YM=",'base64')
+  );
+
+console.log("verified", verified);
