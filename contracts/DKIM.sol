cda4d48e7a302102a3a12c362ce7f7594ec4ea7a
bakaking <tatattai@yahoo.com>
2019-11-07 17:20:39 +0700

    Refactor

--- contracts/DKIM.sol
+++ contracts/DKIM.sol
@@ -59,157 +59,6 @@ contract DKIM is RSASHA256Algorithm{
         return 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCRV9r/XrhF3yRvXjFRRP8RKsT3yqVVrZGFYgsKLl/7exRJJBfIBPI+nRzpC1pu5XGUZaheGtj/m1WDU9TrFK4wIvLvKyX65eePw3wNsUMVJP76baeDtilQaUk55iPKq3hzoRDP+buEj0Plivz8sU3lSvTx/Tk54kcsa5UU8XTpVQIDAQAB'.toSlice();
     }
 
-    function parseSignature(strings.slice signature) private pure returns (
-        strings.slice d,
-        strings.slice s,
-        strings.slice c,
-        strings.slice a,
-        strings.slice h,
-        strings.slice b,
-        strings.slice bh
-    ) {
-        signature.split(": ".toSlice());
-        var sdelim = ";".toSlice();
-        while (!signature.empty()) {
-            var spart = signature.split(sdelim);
-            var tagname = spart.split("=".toSlice());
-            if (tagname.endsWith("d".toSlice())) {
-                d = spart;
-            } else if (tagname.endsWith("s".toSlice())) {
-                s = spart;
-            } else if (tagname.endsWith("c".toSlice())) {
-                c = spart;
-            } else if (tagname.endsWith("a".toSlice())) {
-                a = spart;
-            } else if (tagname.endsWith("bh".toSlice())) {
-                bh = spart;
-            } else if (tagname.endsWith("h".toSlice())) {
-                h = spart;
-            } else if (tagname.endsWith("b".toSlice())) {
-                b = spart;
-            }
-        }
-    }
-
-    function parseTagHeader(strings.slice value) private pure returns (strings.slice[]) {
-        var delim = ":".toSlice();
-        var tagheaders = new strings.slice[](value.count(delim) + 2);
-        for(uint i = 0; i < tagheaders.length; i++) {
-            tagheaders[i] = value.split(delim);
-        }
-        tagheaders[tagheaders.length - 1] = "dkim-signature".toSlice();
-        return tagheaders;
-    }
-
-    function processBody(strings.slice message, strings.slice method) internal pure returns (
-        string
-    ) {
-        var crlf = "\r\n".toSlice();
-        var spsp = "\x20\x20".toSlice();
-        if (method.equals("relaxed".toSlice())) {
-            // Ignore all whitespace at the end of lines.
-            while (message.contains("\x20\r\n".toSlice())) {
-                message = message.split("\x20\r\n".toSlice()).concat(crlf).toSlice().concat(message).toSlice();
-            }
-
-            // Reduce all sequences of WSP within a line to a single SP
-            while (message.contains(spsp)) {
-                message = message.split(spsp).concat("\x20".toSlice()).toSlice().concat(message).toSlice();
-            }
-        }
-
-        // Ignore all empty lines at the end of the message body.
-        var emptyLines = "\r\n\r\n".toSlice();
-        while (message.endsWith(emptyLines)) {
-            message._len -= crlf._len;
-        }
-        return message.toString();
-    }
-
-
-    function trim(strings.slice self) internal pure returns (strings.slice) {
-        while (self.startsWith("\x20".toSlice()) || self.startsWith("\x09".toSlice())) {
-            self._len -= 1;
-            self._ptr += 1;
-        }
-        return self;
-        // uint word;
-        // assembly { word:= mload(mload(add(self, 32))) }
-        // for (uint j = 0; j < 32; j++) {
-        //     byte b = byte(bytes32(uint(word) * 2 ** (8 * j)));
-        //         if (b == 0x20 || b == 0x09) {
-        //             self._ptr++;
-        //             self._len--;
-        //         } else break;
-        // }
-        // return self;
-    }
-
-    function unfold(strings.slice value) internal pure returns (strings.slice) {
-        var delim = "\r\n".toSlice();
-        var count = value.count(delim);
-        if (count == 0) return value;
-        var tagheaders = new strings.slice[](count + 1);
-        for(uint i = 0; i < tagheaders.length; i++) {
-            tagheaders[i] = value.split(delim);
-        }
-        return "".toSlice().join(tagheaders).toSlice();
-    }
-
-    function removeWSPSequences(strings.slice value) internal pure returns (strings.slice) {
-        var sp = "\x20".toSlice();
-        var scount = value.count(sp);
-        if (scount == 0) return value;
-        var sparts = new strings.slice[](scount + 1);
-        for(uint j = 0; j < sparts.length; j++) {
-            sparts[j] = value.split(sp);
-        }
-        return sp.joinNoEmpty(sparts).toSlice();
-    }
-
-    function processHeader(H[] memory newH, strings.slice signatureHeaders, strings.slice method) internal pure returns (
-        string
-    ) {
-        var crlf = "\r\n".toSlice();
-        var colon = ":".toSlice();
-        var tagHeader = parseTagHeader(signatureHeaders);
-        var processedHeader = new strings.slice[](tagHeader.length);
-
-        for (uint j = 0; j < tagHeader.length; j++) {
-            var value = getH(newH, tagHeader[j].toString()).copy();
-            var name = _toLower(value.split(colon).toString()).toSlice();
-
-            // Remove signature value for "dkim-signature" header
-            var p1 = value.split("b=".toSlice());
-            if (value.empty()) {
-                value = p1;
-            } else {
-                p1._len += 2;
-                value.split(";".toSlice());
-                value = p1.concat(value).toSlice();
-            }
-
-            // Unfold all header field continuation lines
-            value = unfold(value);
-            // Convert all sequences of one or more WSP characters to a single SP
-            value = removeWSPSequences(value);
-            // Remove any WSP characters remaining before and after the colon
-            while (value.startsWith("\x20".toSlice())) {
-                value._len -= 1;
-                value._ptr += 1;
-            }
-
-            
-
-            var h = new strings.slice[](2);
-            h[0] = name;
-            h[1] = value;
-            processedHeader[j] = colon.join(h).toSlice();
-        }
-
-        return crlf.join(processedHeader);
-    }
-
     bytes public modulus;
     bytes public exponent;
     function set(bytes m, bytes e) public {
@@ -222,32 +71,30 @@ contract DKIM is RSASHA256Algorithm{
         strings.slice all;
     }
 
-
-    function getH(H[] memory newH, string memory name) internal pure returns (strings.slice) {
-        var hn = _toLower(name).toSlice();
-        for (uint i = 0; i < newH.length; i++) {
-            if (newH[i].name.equals(hn)) return newH[i].all;
+    function getH(H[] memory headers, string memory name) internal pure returns (strings.slice memory) {
+        strings.slice memory headerName = toLowercase(name).toSlice();
+        for (uint i = 0; i < headers.length; i++) {
+            if (headers[i].name.equals(headerName)) return headers[i].all.copy();
         }
-        return "".toSlice();
+        revert("Header not found");
     }
 
     function getLen(string memory raw) public returns (bool) {
-        var (headers, body) = parse(raw.toSlice());
-        var (d, s, c, a, h, b, bh) = parseSignature(getH(headers, "dkim-signature").copy());
-
+        (H[] memory headers, strings.slice memory body) = parse(raw.toSlice());
+        (strings.slice memory d,
+        strings.slice memory s,
+        strings.slice memory c,
+        strings.slice memory a,
+        strings.slice memory h,
+        strings.slice memory b,
+        strings.slice memory bh) = parseHeaderParams(headers);
+                
+        strings.slice memory headerCan = c.split("/".toSlice());
+        if (c.empty()) c = "simple".toSlice();
         bytes32 digest = sha256(bytes(processBody(body, c)));
         if (Base64.decode(bh.toString()).readBytes32(0) != digest) return false;
 
-        var processedHeader = processHeader(headers, h, c);
-
-        var crlf = "\r\n".toSlice();
-        
-        while (b.contains(crlf)) {
-            b = b.split(crlf).concat(b).toSlice();
-        }
-        while (b.contains("\x20".toSlice())) {
-            b = b.split("\x20".toSlice()).concat(b).toSlice();
-        }
+        string memory processedHeader = processHeader(headers, h, headerCan);
         return verify(modulus, exponent, bytes(processedHeader), Base64.decode(b.toString()));
     }
 
@@ -267,7 +114,7 @@ contract DKIM is RSASHA256Algorithm{
                 headerValue._len += crlf._len + part._len;
             } else {
                 if (!headerName.empty()) {
-                    headers[i] = H(_toLower(headerName.toString()).toSlice(), headerValue);
+                    headers[i] = H(toLowercase(headerName.toString()).toSlice(), headerValue);
                     i++;
                 }
                 headerName = part.copy().split(colon);
@@ -283,7 +130,105 @@ contract DKIM is RSASHA256Algorithm{
         revert("No header boundary found");
     }
 
-    function _toLower(string str) internal pure returns (string) {
+    function parseHeaderParams(H[] memory headers) internal pure returns (
+        strings.slice d,
+        strings.slice s,
+        strings.slice c,
+        strings.slice a,
+        strings.slice h,
+        strings.slice b,
+        strings.slice bh
+    ) {
+        strings.slice memory signature = getH(headers, "dkim-signature");
+        strings.slice memory sc = ";".toSlice();
+        strings.slice memory eq = "=".toSlice();
+
+        signature.split(":".toSlice());
+        while (!signature.empty()) {
+            strings.slice memory value = signature.split(sc);
+            strings.slice memory name = trim(value.split(eq));
+            value = trim(value);
+            if (name.equals("d".toSlice())) {
+                d = value;
+            } else if (name.equals("s".toSlice())) {
+                s = value;
+            } else if (name.equals("c".toSlice())) {
+                c = value;
+            } else if (name.equals("a".toSlice())) {
+                a = value;
+            } else if (name.equals("bh".toSlice())) {
+                bh = value;
+            } else if (name.equals("h".toSlice())) {
+                h = value;
+            } else if (name.equals("b".toSlice())) {
+                b = unfoldContinuationLines(value, true);
+            }
+        }
+    }
+
+    function processBody(strings.slice message, strings.slice method) internal pure returns (string) {
+        if (method.equals("relaxed".toSlice())) {
+            message = removeSPAtEndOfLines(message);
+            message = removeWSPSequences(message);
+        }
+        message = ignoreEmptyLineAtEnd(message);
+        return message.toString();
+    }
+
+    function processHeader(H[] memory headers, strings.slice memory h, strings.slice memory method) internal pure returns (string) {
+        strings.slice memory crlf = "\r\n".toSlice();
+        strings.slice memory colon = ":".toSlice();
+        strings.slice[] memory tags = parseTagList(h);
+        strings.slice[] memory processedHeader = new strings.slice[](tags.length);
+        bool isSimple = method.equals("simple".toSlice());
+
+        for (uint j = 0; j < tags.length; j++) {
+            strings.slice memory value = getH(headers, tags[j].toString());
+            if (isSimple) {
+                processedHeader[j] = value;
+                continue;
+            }
+
+            // Convert all header field names to lowercase
+            strings.slice memory name = toLowercase(trim(value.split(colon)).toString()).toSlice();
+
+            // Remove signature value for "dkim-signature" header
+            if (name.equals("dkim-signature".toSlice())) {
+                var part1 = value.split("b=".toSlice());
+                if (value.empty()) {
+                    value = part1;
+                } else {
+                    part1._len += 2;
+                    value.split(";".toSlice());
+                    value = part1.concat(value).toSlice();
+                }
+            }
+
+            value = unfoldContinuationLines(value, false);
+            value = removeWSPSequences(value);
+            value = trim(value);
+
+            strings.slice[] memory parts = new strings.slice[](2);
+            parts[0] = name;
+            parts[1] = value;
+            processedHeader[j] = colon.join(parts).toSlice();
+        }
+
+        return crlf.join(processedHeader);
+    }
+
+    function parseTagList(strings.slice memory value) internal pure returns (strings.slice[]) {
+        strings.slice memory colon = ":".toSlice();
+        strings.slice[] memory list = new strings.slice[](value.count(colon) + 2);
+        for(uint i = 0; i < list.length; i++) {
+            list[i] = trim(value.split(colon));
+        }
+        list[list.length - 1] = "dkim-signature".toSlice();
+        return list;
+    }
+
+    // utils
+    function toLowercase(string str) internal pure returns (string) {
 		bytes memory bStr = bytes(str);
 		for (uint i = 0; i < bStr.length; i++) {
 			if ((bStr[i] >= 65) && (bStr[i] <= 90)) {
@@ -292,4 +237,71 @@ contract DKIM is RSASHA256Algorithm{
 		}
 		return string(bStr);
 	}
+
+    function trim(strings.slice memory self) internal pure returns (strings.slice) {
+        strings.slice memory sp = "\x20".toSlice();
+        strings.slice memory tab = "\x09".toSlice();
+        strings.slice memory crlf = "\r\n".toSlice();
+        if (self.startsWith(crlf)) {
+            self._len -= 2;
+            self._ptr += 2;
+        }
+        while (self.startsWith(sp) || self.startsWith(tab)) {
+            self._len -= 1;
+            self._ptr += 1;
+        }
+        if (self.endsWith(crlf)) {
+            self._len -= 2;
+        }
+        while (self.endsWith(sp) || self.endsWith(tab)) {
+            self._len -= 1;
+        }
+        return self;
+    }
+
+    function removeSPAtEndOfLines(strings.slice memory value) internal pure returns (strings.slice) {
+        strings.slice memory sp = "\x20".toSlice();
+        strings.slice memory crlf = "\r\n".toSlice();
+        uint count = value.count(crlf);
+        if (count == 0) return value;
+        strings.slice[] memory parts = new strings.slice[](count + 1);
+        for(uint j = 0; j < parts.length; j++) {
+            parts[j] = value.split(crlf);
+            while (parts[j].endsWith(sp)) {
+                parts[j]._len -= 1;
+            }
+        }
+        return crlf.join(parts).toSlice();
+    }
+
+    function removeWSPSequences(strings.slice memory value) internal pure returns (strings.slice) {
+        strings.slice memory sp = "\x20".toSlice();
+        uint count = value.count(sp);
+        if (count == 0) return value;
+        strings.slice[] memory parts = new strings.slice[](count + 1);
+        for(uint j = 0; j < parts.length; j++) {
+            parts[j] = value.split(sp);
+        }
+        return sp.joinNoEmpty(parts).toSlice();
+    }
+
+    function ignoreEmptyLineAtEnd(strings.slice memory value) internal pure returns (strings.slice) {
+        strings.slice memory emptyLines = "\r\n\r\n".toSlice();
+        while (value.endsWith(emptyLines)) {
+            value._len -= 2;
+        }
+        return value;
+    }
+
+    function unfoldContinuationLines(strings.slice memory value, bool isTrim) internal pure returns (strings.slice) {
+        strings.slice memory crlf = "\r\n".toSlice();
+        uint count = value.count(crlf);
+        if (count == 0) return value;
+        strings.slice[] memory parts = new strings.slice[](count + 1);
+        for(uint i = 0; i < parts.length; i++) {
+            parts[i] = value.split(crlf);
+            if (isTrim) parts[i] = trim(parts[i]);
+        }
+        return "".toSlice().join(parts).toSlice();
+    }
 }
